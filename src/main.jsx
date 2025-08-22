63a95bb15a8862426d113eec2a04ca93887e70f8
Code-Master20 <sahidurmiah201920@gmail.com>
2025-08-22 10:27:11 -0700

    Update code before deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
+
 import Root from "./Root.jsx";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Projects from "./components/Projects-nav.jsx";
+
 import TodoApp from "./projects/TodoApp/TodoApp.jsx";
 import AddTodo from "./projects/TodoApp/AddTodo.jsx";
 import RemainedTodos from "./projects/TodoApp/RemainedTodos.jsx";
@@ -13,30 +15,25 @@ import CompletedTodos from "./projects/TodoApp/CompletedTodos.jsx";
 import CurrencySwap from "./projects/CurrencySwap/CurrencySwap.jsx";
 import WeatherApp from "./projects/WeatherApp/WeatherApp.jsx";
 
-const isGitHubPages = window.location.hostname === "codemaster20.github.io";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="projects" element={<Projects />}>
-        <Route index element={<Navigate to="weather" replace />} />
-        <Route path="todo" element={<TodoApp />}>
-          <Route index element={<Navigate to="add-todo" replace />} />
-          <Route path="add-todo" element={<AddTodo />} />
-          <Route path="remains" element={<RemainedTodos />} />
-          <Route path="completes" element={<CompletedTodos />} />
-        </Route>
-        <Route path="currency" element={<CurrencySwap />} />
-        <Route path="weather" element={<WeatherApp />} />
-      </Route>
-    </Route>
-  ),
-  { basename: isGitHubPages ? "/weCode" : "/" }
-);
-
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="projects" element={<Projects />}>
+          <Route index element={<Navigate to="weather" replace />} />
+          <Route path="todo" element={<TodoApp />}>
+            <Route index element={<Navigate to="add-todo" replace />} />
+            <Route path="add-todo" element={<AddTodo />} />
+            <Route path="remains" element={<RemainedTodos />} />
+            <Route path="completes" element={<CompletedTodos />} />
+          </Route>
+          <Route path="currency" element={<CurrencySwap />} />
+          <Route path="weather" element={<WeatherApp />} />
+        </Route>
+      </Route>
+    </Routes>
+  </HashRouter>
 );
